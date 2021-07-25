d4a73f7a4513859e5c742a267682771d4bce2c5e
lijiale <1157221853@qq.com>
2021-07-25 23:15:58 +0800

    结构构建

---
+++ src/test/java/com/learn/test/BeanTest.java
@@ -0,0 +1,20 @@
+package com.learn.test;
+
+import org.junit.Test;
+
+/**
+ * Description:
+ * date: 2021/7/25 23:14
+ * Package: com.learn.test
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+public class BeanTest {
+
+    @Test
+    public void test1(){
+        System.out.println("111");
+    }
+
+}
