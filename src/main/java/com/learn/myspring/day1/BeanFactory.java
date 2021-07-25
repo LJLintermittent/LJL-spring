7367681459f49b6e9fcc1997b6a3cd18c067ea2d
lijiale <1157221853@qq.com>
2021-07-25 23:13:09 +0800

    day1 实现一个简单的bean容器

---
+++ src/main/java/com/learn/myspring/day1/BeanFactory.java
@@ -0,0 +1,14 @@
+package com.learn.myspring.day1;
+
+/**
+ * Description:
+ * date: 2021/7/25 23:11
+ * Package: com.learn.myspring.day1
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class BeanFactory {
+
+}
