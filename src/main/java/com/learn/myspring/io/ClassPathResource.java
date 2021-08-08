8d87665aef9f06fd4f86559b1d321afd6c6d70d8
lijiale <1157221853@qq.com>
2021-08-08 23:00:53 +0800

    定义Resource接口，来处理资源加载流。

---
+++ src/main/java/com/learn/myspring/io/ClassPathResource.java
@@ -0,0 +1,43 @@
+package com.learn.myspring.io;
+
+import cn.hutool.core.lang.Assert;
+import com.learn.myspring.utils.ClassUtils;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Description:
+ * date: 2021/8/8 22:54
+ * Package: com.learn.myspring.io
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class ClassPathResource implements Resource {
+
+    private final String path;
+
+    private ClassLoader classLoader;
+
+    public ClassPathResource(String path) {
+        this(path, (ClassLoader) null);
+    }
+
+    public ClassPathResource(String path, ClassLoader classLoader) {
+        Assert.notNull(path, "Path must not be null");
+        this.path = path;
+        this.classLoader = (classLoader != null ? classLoader : ClassUtils.getDefaultClassLoader());
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        InputStream inputStream = classLoader.getResourceAsStream(path);
+        if (inputStream == null) {
+            throw new FileNotFoundException(this.path + "cannot be opened because it does not exist");
+        }
+        return inputStream;
+    }
+}
---
+++ src/main/java/com/learn/myspring/utils/ClassUtils.java
@@ -0,0 +1,27 @@
+package com.learn.myspring.utils;
+
+/**
+ * Description:
+ * date: 2021/8/8 22:58
+ * Package: com.learn.myspring.utils
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class ClassUtils {
+
+    public static ClassLoader getDefaultClassLoader() {
+        ClassLoader classLoader = null;
+        try {
+            classLoader = Thread.currentThread().getContextClassLoader();
+        } catch (Throwable ex) {
+            // Cannot access thread context ClassLoader - falling back to system class loader...
+        }
+        if (classLoader == null) {
+            // No thread context class loader -> use class loader of this class.
+            classLoader = ClassUtils.class.getClassLoader();
+        }
+        return classLoader;
+    }
+}
